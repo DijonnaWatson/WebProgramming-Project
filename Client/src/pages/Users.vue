8571ba700a9af0de4cfdf0493aab9410948787ae
DijonnaWatson <167108324+DijonnaWatson@users.noreply.github.com>
2024-10-25 20:41:30 -0400

    added an Add User button to the Admin pg

--- Client/src/pages/Users.vue
+++ Client/src/pages/Users.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue';
-import { getAllUsers } from '@/models/users';
+import { getAllUsers, type User } from '@/models/users';
 import { defineProps } from 'vue';
 import UsersCard from '@/components/UsersCard.vue';
-import type { User } from '@/models/users';
+
 
 const props = defineProps({
   user: {
@@ -18,45 +18,70 @@ onMounted(() => {
   users.value = data;
 });
 
-const editUser = (user: User) => {
-  console.log('Editing user:', user);//temporary code for button to get rid of warning
+const addUser = (user: User) => {
+  console.log('Add user button clicked', user);//temporary code for button to get rid of warning, does display that editing user works in the inspect console
+  // Implement edit user functionality
+};
+
+const editUser = () => {
+  console.log('Editing user:');//temporary code for button to get rid of warning, does display that editing user works in the inspect console
   // Implement edit user functionality
 };
 
 const deleteUser = (user: User) => {
-  console.log('Deleting user:', user);//temporary code for button to get rid of warning
+  console.log('Deleting user:', user);//temporary code for button to get rid of warning, does display that deleting user works in the inspect console
   // Implement delete user functionality
 };
 </script>
 <template>
   <div>
     <h1>Admin - User List</h1>
-    <table v-if="props.user?.adminAccess">
-      <thead>
-        <tr>
-          <th>Profile Picture</th>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Email</th>
-          <th>Admin Access</th>
-          <th>Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        <UsersCard
-          v-for="user in users"
-          :key="user.email"
-          :user="user"
-          @edit="editUser"
-          @delete="deleteUser"
-        />
-      </tbody>
-    </table>
-    <p v-else>You do not have access to view this page.</p>
+    <div v-if="props.user?.adminAccess"> 
+      <button @click="addUser(user)" class="add-user-button"> 
+        <i class="fas fa-plus"></i> Add User
+      </button>
+      <table>
+        <thead>
+          <tr>
+            <th>Profile Picture</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Admin Access</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <UsersCard
+            v-for="user in users"
+            :key="user.email"
+            :user="user"
+            @edit="editUser"
+            @delete="deleteUser"
+          />
+        </tbody>
+      </table>
+    </div>
+    <p v-else>You do not have access to view this page. Login as Angela to get access</p>
   </div>
 </template>
 
 <style>
+.add-user-button {
+  background-color: #007BFF;
+  color: #fff;
+  border: none;
+  padding: 0.5rem 1rem;
+  border-radius: 0.3rem;
+  cursor: pointer;
+  transition: background-color 0.3s;
+  margin : 0;
+  margin-top:1.5rem;
+}
+
+.add-user-button:hover {
+  background-color: #0056b3;
+}
 .profile-pic {
   width: 5.5rem;
   height: 3.5rem;
@@ -65,7 +90,7 @@ const deleteUser = (user: User) => {
 table {
   width: 100%;
   border-collapse: collapse;
-  margin-top: 1.5rem;
+  margin-top: 0;
 }
 th, td {
   border: 1px solid #ddd;
