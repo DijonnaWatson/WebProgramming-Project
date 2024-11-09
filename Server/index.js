2df6d6a8598097ed1d0ccb355fbaa3f6bb226b01
DijonnaWatson <167108324+DijonnaWatson@users.noreply.github.com>
2024-11-09 18:20:56 -0500

    Got the npm run dev to work on Server, with the Hello World Displayed for now

--- Server/index.js
+++ Server/index.js
@@ -1,18 +1,26 @@
 // server.mjs
 const express = require("express");
 const app = express(); /*create instance of the express pipeline, everytime a message comes in to the port, the operating system gives the message to express and express goes to find out which function it should run*/
-const userController = require("./controllers/users");
+// const userController = require("./controllers/users");
 
 const PORT = 3000;
 
-const server = createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello New Paltz\n');
-});
+app.get("/", (req, res) => {
+  res.send("Hello World");
+})
 
-// starts a simple http server locally on port 3000
-server.listen(PORT, '127.0.0.1', () => {
-    console.log('Listening on 127.0.0.1:3000');
-});
+app.listen(PORT, () => {
+  console.log("Server is running at http://localhost:" + PORT);
+})
+
+// const server = createServer((req, res) => {
+//     res.writeHead(200, { 'Content-Type': 'text/plain' });
+//     res.end('Hello New Paltz\n');
+// });
+
+// // starts a simple http server locally on port 3000
+// server.listen(PORT, '127.0.0.1', () => {
+//     console.log('Listening on 127.0.0.1:3000');
+// });
 
 // run with `node server.mjs`
